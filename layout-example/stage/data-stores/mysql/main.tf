78608bd13d73025ca71394f88dfadd336868fef8
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-03-27 13:57:23 +0000

    chore: add file layout isolation example

---
+++ layout-example/stage/data-stores/mysql/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-demo-2022-03-26"
+    key    = "stage/data-stores/mysql/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  db_name             = "example_database"
+  username            = "admin"
+  skip_final_snapshot = true
+
+  # How should we set the password?
+  password = var.db_password
+}
