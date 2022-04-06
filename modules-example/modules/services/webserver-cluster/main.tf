f9bffce5c4fa2c3fd1222c4eb74f36a2e4ee57f2
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-04-06 13:38:10 +0000

    chore: separate sg rule for instance

--- modules-example/modules/services/webserver-cluster/main.tf
+++ modules-example/modules/services/webserver-cluster/main.tf
@@ -90,13 +90,16 @@ resource "aws_security_group" "instance" {
   tags = {
     "Name" = "${var.cluster_name}-instance"
   }
+}
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ipv4s
-  }
+resource "aws_security_group_rule" "allow_http_traffic_into_instance" {
+  type              = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port   = var.server_port
+  to_port     = var.server_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ipv4s
 }
 
 resource "aws_security_group" "alb" {
@@ -107,22 +110,26 @@ resource "aws_security_group" "alb" {
   tags = {
     "Name" = "${var.cluster_name}-alb"
   }
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = local.http_port
-    to_port     = local.http_port
-    protocol    = local.tcp_protocol
-    cidr_blocks = local.all_ipv4s
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  # Allow all outbound requests
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ipv4s
-  }
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ipv4s
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ipv4s
 }
 
 data "aws_vpc" "default" {
--- modules-example/modules/services/webserver-cluster/outputs.tf
+++ modules-example/modules/services/webserver-cluster/outputs.tf
@@ -6,3 +6,8 @@ output "asg_name" {
   value       = aws_autoscaling_group.example.name
   description = "Name of Auto Scaling Group"
 }
+
+output "alb_security_group_id" {
+  value       = aws_security_group.alb.id
+  description = "ID of security group attached to the Load Balancer"
+}
